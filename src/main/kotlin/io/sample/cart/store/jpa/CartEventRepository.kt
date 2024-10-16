f652b3af72caf7fc910fe5a45e525520c396ffe9
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-16 18:04:30 +0900

    feat: 이벤트 등록 시간을 기준으로 오름차순으로 불러오는 메서드 작성

--- src/main/kotlin/io/sample/cart/store/jpa/CartEventRepository.kt
+++ src/main/kotlin/io/sample/cart/store/jpa/CartEventRepository.kt
@@ -2,4 +2,6 @@ package io.sample.cart.store.jpa
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CartEventRepository : JpaRepository<CartEventJpo, String> {}
+interface CartEventRepository : JpaRepository<CartEventJpo, String> {
+    fun findByCartIdOrderByTimeAsc(cartId: String): List<CartEventJpo>
+}
